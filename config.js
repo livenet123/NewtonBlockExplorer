8eef17c36464d82d9bf86239e9c3f0838de04ccc
livenet123 <17086936+livenet123@users.noreply.github.com>
2018-04-06 00:49:49 +0300

    initial commmit

---
+++ config.js
@@ -0,0 +1,12 @@
+var api = 'http://185.45.192.242:21236';
+var blockTargetInterval = 240;
+var coinUnits = 100000000;
+var symbol = 'NCP';
+var refreshDelay = 30000;
+// pools stats by MainCoins
+var networkStat = {
+    "NCP": [
+		["www.newtoncoin.club", "http://www.newtoncoin.club:8117/stats"],
+                ["ncp.efectivepool.tk", "http://ncp.efectivepool.tk:8117/stats"],				
+    ]
+};
